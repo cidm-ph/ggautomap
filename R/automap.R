c60a54008a570f194949ecec3dcb661eb071ca1f
Carl Suster <Carl.Suster@health.nsw.gov.au>
2023-04-12 16:28:48 +1000

    Rename into_sf -> automap

--- R/automap.R
+++ R/automap.R
@@ -15,11 +15,11 @@
 #' @examples
 #' data(nc_type_example)
 #'
-#' into_sf(nc_type_example, location)
-into_sf <- function(x, location, feature_type = NA, geom_name = "geometry") {
+#' automap(nc_type_example, location)
+automap <- function(x, location, feature_type = NA, geom_name = "geometry") {
   location_data <- dplyr::pull(x, {{ location }})
   feature_type <- resolve_feature_type(feature_type, location_data,
-                                       context = "{.fn into_sf}")
+                                       context = "{.fn automap}")
   location_data <- resolve_feature_names(location_data, feature_type)
 
   matches <- match(location_data, get_feature_names(feature_type))
