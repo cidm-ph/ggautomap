9f735239782ac5d0237ee946de2904894046a122
Carl Suster <Carl.Suster@health.nsw.gov.au>
2023-04-14 10:14:29 +1000

    Add stat_automap

---
+++ R/stat_automap.R
@@ -0,0 +1,84 @@
+#' Attach spatial data with cartographer
+#'
+#' Use cartographer to attach a spatial column to the data based on place names
+#' in another column. The result can then be used by [ggplot2::geom_sf()] or
+#' [ggmapinset::geom_sf_inset()].
+#'
+#' @section Computed variables:
+#' \describe{
+#'   \item{geometry}{\code{sf} geometry column}
+#'   \item{...}{limits as computed by [ggplot2::stat_sf()]}
+#' }
+#'
+#' @param mapping,data,stat,geom,position,na.rm,show.legend,inherit.aes,... See [ggplot2::geom_sf()].
+#' @inheritParams cartographer::resolve_feature_type
+#'
+#' @export
+#' @returns A plot layer
+#' @examples
+#' library(ggplot2)
+#'
+#' events <- data.frame(
+#'   county = c("Mecklenburg", "Carteret", "Moore", "Caldwell"),
+#'   proportion_A = c(0.1, 0.8, 0.0, 0.6))
+#'
+#' ggplot(events, aes(location = county)) +
+#'   geom_sf(aes(fill = proportion_A), stat = "automap")
+#'
+#' ggplot(events, aes(location = county)) +
+#'   stat_automap(aes(fill = proportion_A)) +
+#'   coord_sf()
+stat_automap <- function(mapping = NULL, data = NULL,
+                         geom = "sf", position = "identity",
+                         ...,
+                         feature_type = NA,
+                         na.rm = TRUE,
+                         show.legend = NA,
+                         inherit.aes = TRUE) {
+  ggplot2::layer_sf(
+    data = data,
+    mapping = mapping,
+    stat = StatAutomap,
+    geom = geom,
+    position = position,
+    show.legend = show.legend,
+    inherit.aes = inherit.aes,
+    params = rlang::list2(
+      feature_type = feature_type,
+      na.rm = na.rm,
+      ...
+    )
+  )
+}
+
+#' @rdname stat_automap
+#' @usage NULL
+#' @format NULL
+#'
+#' @export
+StatAutomap <- ggplot2::ggproto("StatAutomap", ggplot2::StatSf,
+  required_aes = c("location"),
+
+  setup_data = function(data, params) {
+    data <- ggplot2::StatSf$setup_data(data, params)
+    data$location <- cartographer::resolve_feature_names(data$location,
+                                                         params$feature_type)
+    data
+  },
+
+  setup_params = function(data, params) {
+    params <- ggplot2::StatSf$setup_params(data, params)
+    if (is.null(params[["feature_type"]])) params$feature_type <- NA
+    params$feature_type <- cartographer::resolve_feature_type(params$feature_type,
+                                                              data$location)
+    params
+  },
+
+  compute_panel = function(data, scales, coord, feature_type) {
+    geoms <- cartographer::map_sfc(data$location, feature_type)
+    crs_data <- sf::st_crs(cartographer::map_sf(feature_type))
+    data$geometry <- sf::st_sfc(geoms, crs = crs_data)
+
+    ggplot2::StatSf$compute_panel(sf::st_as_sf(data), scales, coord)
+  }
+)
