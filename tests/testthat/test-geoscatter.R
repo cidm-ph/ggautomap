f7f47f9601249969775ee91ce6335cb92f6cdb62
Carl Suster <carl.suster@sydney.edu.au>
2026-03-25 13:52:20 +1100

    Don't hard-code test assumptions about dataset

--- tests/testthat/test-geoscatter.R
+++ tests/testthat/test-geoscatter.R
@@ -2,10 +2,10 @@ test_that("geoscatter is not silently duplicated", {
   library(ggplot2)
   library(nswgeo)
 
+  points <- dplyr::filter(covid_cases_nsw, lga == "Walgett")
+
   plot <-
-    covid_cases_nsw |>
-    dplyr::filter(lga == "Walgett") |>
-    ggplot(aes(location = lga)) +
+    ggplot(points, aes(location = lga)) +
     # geom_boundaries(feature_type = "nswgeo.lga") +
     geom_geoscatter(aes(colour = type), sample_type = "random", size = 0.5) +
     coord_automap(
@@ -16,10 +16,10 @@ test_that("geoscatter is not silently duplicated", {
     guides(colour = guide_legend(override.aes = list(size = 1))) +
     theme_void()
 
-  # The dataset contains 5 points in Walgett. We expect 2 plot layers -
-  # one for the base containing a points grob with 5 points, and one
-  # for the inset which is a nullGrob since the inset was not configured.
+  # We expect 2 plot layers: one for the base containing a points grob with
+  # exactly the points in `points`, and one for the inset which is a
+  # nullGrob since the inset was not configured.
   expect_length(plot@layers, 2)
-  expect_length(ggplot2::get_layer_grob(plot, 1)[[1]]$x, 5)
+  expect_length(ggplot2::get_layer_grob(plot, 1)[[1]]$x, nrow(points))
   expect_s3_class(ggplot2::get_layer_grob(plot, 2)[[1]], "null")
 })
